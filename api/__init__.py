4d477b25dabda9a5106f8d658523a786bc4d0688
Kelly Truesdale <kelly.truesdale@gmail.com>
2016-04-21 22:54:05 -0400

    Switching to local debug server instead of uwsgi.

--- api/__init__.py
+++ api/__init__.py
@@ -21,6 +21,3 @@ def cleanup_db_session(exc=None):
 @flask_app.route('/')
 def root():
     return flask_app.send_static_file('index.html')
-
-if __name__ == '__main__':
-    flask_app.run('0.0.0.0', 5000, debug=True)
