8b9b6a64d9320db4c9af7d36e30cf70e08bb5318
Aaron Buxbaum <me@aaronbuxbaum.com>
2016-04-23 14:21:08 -0400

    Hook up the log in service

--- src/login/login-ctrl.js
+++ src/login/login-ctrl.js
@@ -2,9 +2,16 @@ angular.module('CS6310').controller('LoginCtrl', function (UserService) {
   var ctrl = this;
 
   ctrl.logIn = function () {
-    console.log('TODO');
+    return UserService.logIn(ctrl.username, ctrl.password).then(function (userData) {
+      var route = {
+        'student': 'Student Registration',
+        'administrator': 'Administrator Panel',
+        'ta': 'Teaching Assistant'
+      };
 
-    UserService.loggedIn = true;
-    ctrl.$router.navigate(['Student Registration', { id: 0 }]);
+      return ctrl.$router.navigate([route[userData.role], { id: userData.id }]);
+    }, function (error) {
+      ctrl.errorMessage = error.statusText;
+    });
   };
 });
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,5 +1,16 @@
-angular.module('CS6310').factory('UserService', function () {
+angular.module('CS6310').factory('UserService', function ($http) {
   var svc = {};
-  
+  var url = 'http://192.168.99.104/api/user/';
+
+  svc.logIn = function (username, password) {
+    return $http.post(url + 'login', {
+      username: username,
+      password: password
+    }).then(function (response) {
+      svc.loggedIn = response.data;
+      return svc.loggedIn;
+    });
+  };
+
   return svc;
 });
