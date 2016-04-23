a45a7f3d30b2eea6f6b8df8937d373d6f1523e82
Aaron Buxbaum <me@aaronbuxbaum.com>
2016-04-23 16:24:00 -0400

    Update demand service

--- src/services/demand-service.js
+++ src/services/demand-service.js
@@ -2,7 +2,7 @@ angular.module('CS6310').factory('DemandService', function (API_URL, $http) {
   var svc = {};
   var url = API_URL + 'demand/';
 
-  svc.getDemand = function() {
+  svc.getDemand = function () {
     return $http.get(url);
   };
 
@@ -11,12 +11,7 @@ angular.module('CS6310').factory('DemandService', function (API_URL, $http) {
   };
 
   svc.submitDemand = function (selectedClasses) {
-    return $http.post(url, selectedClasses.map(function (item) {
-      return {
-        semester_id: 1,
-        course_id: item.id
-      };
-    }));
+    return $http.post(url, { courses: selectedClasses });
   };
 
   return svc;
