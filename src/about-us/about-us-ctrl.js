b967f3054bb60257738dc0d383477bc4c74b1f49
Aaron Buxbaum <me@aaronbuxbaum.com>
2016-04-24 18:09:35 -0400

    Update about us

--- src/about-us/about-us-ctrl.js
+++ src/about-us/about-us-ctrl.js
@@ -1,35 +1,14 @@
 angular.module('CS6310').controller('AboutUsCtrl', function () {
     var ctrl = this;
 
-    var tempDevelopers = [
+    var developers = [
         {
             name: 'Aaron Buxbaum',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
-            image: 'http://mediad.publicbroadcasting.net/p/kwmu/files/201508/tacos.jpg',
+            text: 'I am a Web Developer at Google, on the Maps Local Guides program. I specialize in AngularJS and Javascript.',
+            image: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/2/005/01c/0ac/0ece994.jpg',
             link: 'https://www.linkedin.com/in/aaronbuxbaum'
-        },
-        {
-            name: 'Aaron Buxbaum',
-            text: 'AAA',
-            image: 'http://mediad.publicbroadcasting.net/p/kwmu/files/201508/tacos.jpg'
-        },
-        {
-            name: 'Aaron Buxbaum',
-            text: 'AAA',
-            image: 'http://mediad.publicbroadcasting.net/p/kwmu/files/201508/tacos.jpg'
-        },
-        {
-            name: 'Aaron Buxbaum',
-            text: 'AAA',
-            image: 'http://mediad.publicbroadcasting.net/p/kwmu/files/201508/tacos.jpg'
-        },
-        {
-            name: 'Aaron Buxbaum',
-            text: 'AAA',
-            image: 'http://mediad.publicbroadcasting.net/p/kwmu/files/201508/tacos.jpg',
-            link: 'http://www.google.com'
         }
     ];
 
-    ctrl.developers = _.shuffle(tempDevelopers);
+    ctrl.developers = _.shuffle(developers);
 });
